fc952bb1fd0a67885b16f330c78234ec668a5ac8
Muhammad Faisal Hayat <faisal.hayat@routetrading.co.uk>
2025-03-11 15:18:06 +0500

    migrations done

--- my_tutorial/urls.py
+++ my_tutorial/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers 
 
-urlpatterns = [
+router = routers.DefaultRouter()
+urlpatterns = router.urls
+
+urlpatterns += [
     path('admin/', admin.site.urls),
 ]
