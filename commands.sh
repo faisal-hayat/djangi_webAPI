dc3c05efffbe31d9e472b814316879b73a704259
Muhammad Faisal Hayat <faisal.hayat@routetrading.co.uk>
2025-03-11 14:33:35 +0500

    requirements.txt has been added

---
+++ commands.sh
@@ -0,0 +1,115 @@
+# commmon commands in django
+python manage.py runserver
+
+django-admin startproject mysite
+
+
+django-admin startproject mysite
+python manage.py startapp polls
+python manage.py createsuperuser
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic
+python manage.py test
+python manage.py shell
+
+python manage.py check --deploy
+
+django-admin startproject mysite
+python manage.py startapp polls
+python manage.py makemigrations polls
+python manage.py migrate
+
+python manage.py createsuperuser
+python manage.py collectstatic
+python manage.py changepassword admin
+
+# run shell
+python manage.py shell
+
+# to create a new app
+python manage.py startapp <app_name>
+
+# to run the server
+python manage.py runserver
+
+# to create a superuser
+python manage.py createsuperuser
+
+# to create a new model
+python manage.py makemigrations <app_name>
+
+# to apply the changes to the database
+python manage.py migrate
+
+# to check the sql query for a migration
+python manage.py sqlmigrate <app_name> <migration_id>
+
+# to check the status of the migrations
+python manage.py showmigrations
+
+# to check the list of migrations
+python manage.py showmigrations <app_name>
+
+# to run the shell
+python manage.py shell
+
+# to run the test
+python manage.py test <app_name>
+
+# to load the fixtures
+python manage.py loaddata <fixture_name>
+
+# to dump the fixtures
+python manage.py dumpdata <app_name>
+
+# to create a new project
+django-admin startproject <project_name>
+
+# to create a new app
+python manage.py startapp <app_name>
+
+# to run the server
+python manage.py runserver
+
+# to create a superuser
+python manage.py createsuperuser
+
+# to create a new model
+python manage.py makemigrations <app_name>
+
+# to apply the changes to the database
+python manage.py migrate
+
+# to check the sql query for a migration
+python manage.py sqlmigrate <app_name> <migration_id>
+
+# to check the status of the migrations
+python manage.py showmigrations
+
+# to check the list of migrations
+python manage.py showmigrations <app_name>
+
+# to run the shell
+python manage.py shell
+
+# to run the test
+python manage.py test <app_name>
+
+# to load the fixtures
+python manage.py loaddata <fixture_name>
+
+# to dump the fixtures
+python manage.py dumpdata <app_name>
+
+# to create a new project
+django-admin startproject <project_name>
+
+# to create a new app
+python manage.py startapp <app_name>
+
+# to run the server
+python manage.py runserver
+
+# to create a superuser
+python manage.py createsuperuser
